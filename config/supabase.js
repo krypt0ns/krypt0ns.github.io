ff7b8bdcdd8910790d87823507f563ada1b533fd
krypt0ns <agent.screw.dose@myclkd.email>
2025-01-21 14:19:49 -0800

    Add files via upload

--- config/supabase.js
+++ config/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://jgdkrezxgwhcaognnjlq.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZGtyZXp4Z3doY2FvZ25uamxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5MjE2MDAsImV4cCI6MjAyMjQ5NzYwMH0.Uh9qXuuA1A9W8e3FzXHrdRoqQA2I2GWX--dqL4s_KQo';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
