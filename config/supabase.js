961e15751f0b5fb18668f56f61d8009c2f0642ba
krypt0ns <agent.screw.dose@myclkd.email>
2025-01-21 14:30:59 -0800

    Add files via upload

--- config/supabase.js
+++ config/supabase.js
@@ -1,6 +1,12 @@
 const supabaseUrl = 'https://jgdkrezxgwhcaognnjlq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZGtyZXp4Z3doY2FvZ25uamxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5MjE2MDAsImV4cCI6MjAyMjQ5NzYwMH0.Uh9qXuuA1A9W8e3FzXHrdRoqQA2I2GWX--dqL4s_KQo';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZGtyZXp4Z3doY2FvZ25uamxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0OTAyMjgsImV4cCI6MjA1MzA2NjIyOH0.kNkduc3dqYZJUWD5_5F3UcoQ-aP1jr9jrrADyJ45CGc';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true
+    }
+});
 
 export { supabase };
