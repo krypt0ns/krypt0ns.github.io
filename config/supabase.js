1518d3001ca868fd21f7a5e10b58b3b946cada1e
krypt0ns <agent.screw.dose@myclkd.email>
2025-01-21 15:25:05 -0800

    Add files via upload

--- config/supabase.js
+++ config/supabase.js
@@ -1,9 +1,9 @@
-const supabaseConfig = {
-    url: 'https://jgdkrezxgwhcaognnjlq.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZGtyZXp4Z3doY2FvZ25uamxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0OTAyMjgsImV4cCI6MjA1MzA2NjIyOH0.kNkduc3dqYZJUWD5_5F3UcoQ-aP1jr9jrrADyJ45CGc'
-};
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
+
+const supabaseUrl = 'https://jgdkrezxgwhcaognnjlq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpnZGtyZXp4Z3doY2FvZ25uamxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0OTAyMjgsImV4cCI6MjA1MzA2NjIyOH0.kNkduc3dqYZJUWD5_5F3UcoQ-aP1jr9jrrADyJ45CGc'
 
 // Initialize Supabase client
-const supabase = supabase.createClient(supabaseConfig.url, supabaseConfig.key);
+const supabase = createClient(supabaseUrl, supabaseKey)
 
-export { supabase }; 
+export { supabase } 
